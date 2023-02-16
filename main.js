44c4e78bcc4dddd4627c036daf1aefcec6841942
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-02-16 21:12:53 +0000

    get date from api and put them into the html

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ const divOption = document.querySelectorAll('.div-option');
 const btnLetter = document.querySelector('.btn-letter');
 const answer = document.querySelectorAll('.answer');
 const btnSubmit = document.querySelector('.btn-submit');
-let counter = 0;
+let counter = 1;
 const apiUrl = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
-
+let selected;
+let arrayPosition = 0;
+let data;
 
 counterStart.innerText = 1;
+numberQuestion.innerText = '1 .';
+selectedOption()
+
 const getData = async () => {
     const apiUrl = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
     const apiReturn = await fetch(apiUrl);
@@ -20,30 +25,43 @@ const getData = async () => {
     return data;
 
 };
-const showData = async () => {
-    try {
-        const data = await getData();
-        question.innerText = `${data.results[0].question}`;
-        answer[0].innerText = `${data.results[0].correct_answer}`;
-        ;
-    } catch (error) {
-        errorMsg.innerText = `choose a option`;
+const showData = () => {
+    question.innerText = `${data.results[arrayPosition].question}`;
+    // 3 dots are to put into array one at a time / created a new [] to put correct and incorrect answers and sort them; 
+    const allAnswers = [...data.results[arrayPosition].incorrect_answers, data.results[arrayPosition].correct_answer].sort();
+    for (let j = 0; j < allAnswers.length; j++) {
+        answer[j].innerText = `${allAnswers[j]}`;
     }
 };
-showData(apiUrl);
 
-divOption.forEach((div) => {
-    div.addEventListener('click', () => {
-        div.classList.add('shadow');
-        div.style.backgroundColor = '#ff735c';
-        console.log(div)
+//result is the return from api
+getData().then(result => {
+    data = result;
+    showData();
+});
+
+
+function selectedOption() {
+    divOption.forEach((div) => {
+        let active = div.addEventListener('click', (e) => {
+            selected = e.target;
+            console.log(selected.innerText);
+            div.classList.add('active-answer');
+
+        })
+        if (active) {
+            div.classList.remove('active-answer');
+        }
     })
-})
+}
 
 function nextQuestion() {
+    arrayPosition = arrayPosition + 1;
+    showData()
     if (counterStart.innerText !== '10') {
         counter = counter + 1;
         counterStart.innerText = counter;
+        numberQuestion.innerText = `${counter} .`;
     }
 }
 
